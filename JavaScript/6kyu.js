f89665252594137ad21916f474f36e18b548c49e
Sergey Bakhar <b113.bahar@gmail.com>
2020-06-03 15:52:34 +0300

    Add "Duplicate Encoder" 6 kyu

--- JavaScript/6kyu.js
+++ JavaScript/6kyu.js
@@ -1,4 +1,30 @@
 
+
+// ======================
+// Duplicate Encoder
+
+// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+// Examples
+// "din"      =>  "((("
+// "recede"   =>  "()()()"
+// "Success"  =>  ")())())"
+// "(( @"     =>  "))((" 
+
+function duplicateEncode(word) {
+  const wordLowerCased = word.toLowerCase();
+  let res = '';
+  for (const letter of wordLowerCased) {
+    if (wordLowerCased.indexOf(letter) === wordLowerCased.lastIndexOf(letter)) {
+      res += '(';
+    } else {
+      res += ')';
+    }
+  }
+  return res;
+}
+
+
 // ======================
 // Take a Ten Minute Walk
 
