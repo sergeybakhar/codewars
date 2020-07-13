692cd47231927250f2aa56f255eaeb60eb9006a3
Sergey Bakhar <b113.bahar@gmail.com>
2020-07-13 17:21:06 +0300

    If you can"t sleep, just count sheep!!, 8kyu, TypeScript

--- TypeScript/8kyu.ts
+++ TypeScript/8kyu.ts
@@ -1,4 +1,21 @@
 // ======================
+// If you can't sleep, just count sheep!!
+
+// Task:
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+function countSheep(num: number): string {
+  let counter: number = 1;
+  let sheepTxt: string = "";
+
+  while (counter <= num) {
+    sheepTxt += `${counter} sheep...`;
+    counter++;
+  }
+  return sheepTxt;
+}
+
+// ======================
 // Opposite number
 // Very simple, given a number, find its opposite.
 
